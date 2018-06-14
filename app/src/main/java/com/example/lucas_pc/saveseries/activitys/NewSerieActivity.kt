63c5244738e34a26b10c289528167a0f5078dc4c
CaladoLucas <lcaraujo4252@gmail.com>
2018-06-14 20:27:21 -0300

    start activity result

--- app/src/main/java/com/example/lucas_pc/saveseries/activitys/NewSerieActivity.kt
+++ app/src/main/java/com/example/lucas_pc/saveseries/activitys/NewSerieActivity.kt
@@ -3,9 +3,12 @@ package com.example.lucas_pc.saveseries.activitys
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.lucas_pc.saveseries.R
 import com.example.lucas_pc.saveseries.models.Serie
 import com.example.lucas_pc.saveseries.util.ListaDeSeries
+import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_new_serie.*
 
 class NewSerieActivity : AppCompatActivity() {
@@ -19,9 +22,24 @@ class NewSerieActivity : AppCompatActivity() {
 
                 val list = ListaDeSeries.LISTA.add(Serie(edtName.text.toString(), edtDescricao.text.toString()))
 
-                val intent = Intent(this, PrincipalActivity::class.java)
-                intent.putExtra("lista", list)
-                startActivity(intent)
+                if(intent.extras == null){
+
+                    Log.d("IF","to no normal")
+                    val intent = Intent(this, PrincipalActivity::class.java)
+                    intent.putExtra("lista", list)
+                    intent.putExtra("Nome",edtName.text.toString())
+                    startActivity(intent)
+                }else{
+                    Log.d("IF","to no result")
+                    val resultIntent = Intent()
+                    resultIntent.putExtra("lista", list)
+                    resultIntent.putExtra("Nome",edtName.text.toString())
+                    setResult(Intent.FILL_IN_ACTION,resultIntent)
+                    finish()
+                }
+
+
+
             }
         }
     }
--- app/src/main/java/com/example/lucas_pc/saveseries/activitys/PrincipalActivity.kt
+++ app/src/main/java/com/example/lucas_pc/saveseries/activitys/PrincipalActivity.kt
@@ -3,6 +3,7 @@ package com.example.lucas_pc.saveseries.activitys
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.lucas_pc.saveseries.R
 import com.example.lucas_pc.saveseries.fragments.ItemFragment
 import com.example.lucas_pc.saveseries.util.ListaDeSeries
@@ -21,8 +22,28 @@ class PrincipalActivity : AppCompatActivity() {
 
 
         floatingActionButton_nova_serie.setOnClickListener {
-            startActivityForResult(Intent(this, NewSerieActivity::class.java), 2)
+            val intent = Intent(this, NewSerieActivity::class.java)
+            intent.putExtra("enviando","1")
+            startActivityForResult(intent, Intent.FILL_IN_ACTION)
         }
 
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(resultCode){
+            Intent.FILL_IN_ACTION->{
+
+                var serieSalvar = data!!.extras["Nome"].toString()
+
+
+                Toast.makeText(this, "Serie Salva: $serieSalvar", Toast.LENGTH_LONG).show()
+
+                supportFragmentManager
+                        .beginTransaction()
+                        .add(R.id.conteiner, ItemFragment.newInstance(ListaDeSeries.LISTA), "ItemFragment")
+                        .commit()
+
+            }
+        }
     }
 }
